cfd9d06e573e9634d6a9f57c18dd1b029eb51595
Ruslan Egorov <r-egorov@outlook.com>
2022-06-09 13:08:20 +0300

    Test parents are categories in imports

--- backend/tests/schemas/test_imports.py
+++ backend/tests/schemas/test_imports.py
@@ -28,25 +28,27 @@ class TestImportSchemas:
     @staticmethod
     def assert_eq_unit_dict_and_schema(unit_dict, unit_schema: ShopUnitImportSchema):
         unit_schema_parentId = unit_schema.parentId if unit_schema is not None else None
-        assert unit_dict["id"] == str(unit_schema.id)
-        assert unit_dict["name"] == unit_schema.name
-        assert unit_dict["parentId"] == unit_schema_parentId
-        assert unit_dict["type"] == str(unit_schema.type.value)
-        assert unit_dict["price"] == unit_schema.price
+        assert unit_dict['id'] == str(unit_schema.id)
+        assert unit_dict['name'] == unit_schema.name
+        assert unit_dict['parentId'] == unit_schema_parentId
+        assert unit_dict['type'] == str(unit_schema.type.value)
+        assert unit_dict['price'] == unit_schema.price
 
     def test_shop_unit_import_schema_no_price_in_offer(self):
         unit = generate_unit_dict(price=None)
-        with pytest.raises(ValidationError):
+        with pytest.raises(ValidationError,
+                           match='categories must not have a price, offers must have a price'):
             ShopUnitImportSchema(**unit)
 
     def test_shop_unit_import_schema_price_in_category(self):
         unit = generate_unit_dict(type_='CATEGORY')
-        with pytest.raises(ValidationError):
+        with pytest.raises(ValidationError,
+                           match='categories must not have a price, offers must have a price'):
             ShopUnitImportSchema(**unit)
 
     def test_shop_unit_import_schema_empty_name(self):
         unit = generate_unit_dict(name='')
-        with pytest.raises(ValidationError):
+        with pytest.raises(ValidationError, match='name must not be empty'):
             ShopUnitImportSchema(**unit)
 
     def test_shop_unit_import_schema_ok(self):
@@ -60,9 +62,9 @@ class TestImportSchemas:
 
     def test_import_schema_ok(self):
         upd_date = datetime.now() - timedelta(days=10)
-        category = generate_unit_dict(id_=uuid.uuid4(), type_="CATEGORY",
+        category = generate_unit_dict(id_=uuid.uuid4(), type_='CATEGORY',
                                       price=None)
-        offer = generate_unit_dict(id_=uuid.uuid4(), type_="OFFER")
+        offer = generate_unit_dict(id_=uuid.uuid4(), type_='OFFER')
 
         import_schema = ImportSchema(
             updateDate=upd_date,
@@ -71,10 +73,10 @@ class TestImportSchemas:
 
     def test_import_schema_date_in_future(self):
         future = datetime.now() + timedelta(days=10)
-        category = generate_unit_dict(id_=uuid.uuid4(), type_="CATEGORY",
+        category = generate_unit_dict(id_=uuid.uuid4(), type_='CATEGORY',
                                       price=None)
-        offer = generate_unit_dict(id_=uuid.uuid4(), type_="OFFER")
-        with pytest.raises(ValidationError):
+        offer = generate_unit_dict(id_=uuid.uuid4(), type_='OFFER')
+        with pytest.raises(ValidationError, match='updateDate cannot be in the future'):
             ImportSchema(
                 updateDate=future,
                 items=[category, offer],
@@ -83,13 +85,27 @@ class TestImportSchemas:
     def test_import_schema_not_unique_ids(self):
         upd_date = datetime.now() - timedelta(days=10)
         id_ = uuid.uuid4()
-        category = generate_unit_dict(id_=id_, type_="CATEGORY",
+        category = generate_unit_dict(id_=id_, type_='CATEGORY',
                                       price=None)
-        offer = generate_unit_dict(id_=id_, type_="OFFER")
+        offer = generate_unit_dict(id_=id_, type_='OFFER')
 
-        with pytest.raises(ValidationError):
+        with pytest.raises(ValidationError, match=f'id {id_} is not unique'):
             ImportSchema(
                 updateDate=upd_date,
                 items=[category, offer],
             )
+    
+    def test_import_schema_offer_as_parent(self):
+        upd_date = datetime.now() - timedelta(days=10)
+        category = generate_unit_dict(id_=uuid.uuid4(), type_='CATEGORY',
+                                      price=None)
+        offer_id = uuid.uuid4()
+        offer = generate_unit_dict(id_=offer_id, type_='OFFER')
+        child_offer = generate_unit_dict(id_=uuid.uuid4(), type_='OFFER', parent_id=offer_id)
 
+        with pytest.raises(ValidationError, match='only categories can be parents'):
+            ImportSchema(
+                updateDate=upd_date,
+                items=[category, offer, child_offer],
+            )
+        
--- backend/yashop/api/schema.py
+++ backend/yashop/api/schema.py
@@ -17,21 +17,21 @@ class ShopUnitImportSchema(BaseModel):
     @validator('name')
     def name_not_empty(cls, name):
         if len(name) < 1:
-            raise ValueError("name must not be empty")
+            raise ValueError('name must not be empty')
         return name
 
     @validator('price')
     def price_only_in_offers(cls, price, values):
-        unit_type = values["type"]
+        unit_type = values['type']
         if (unit_type == ShopUnitType.OFFER and price is None
                 or unit_type == ShopUnitType.CATEGORY and price is not None):
             raise ValueError(
-                "categories must not have a price, offers must have a price"
+                'categories must not have a price, offers must have a price'
             )
         if price is not None:
             if price < 1:
                 raise ValueError(
-                    "price must be positive int, more than one"
+                    'price must be positive int, more than one'
                 )
         return price
 
@@ -45,7 +45,7 @@ class ImportSchema(BaseModel):
     def date_not_future(cls, update_date: datetime):
         if update_date.timestamp() > datetime.now().timestamp():
             raise ValueError(
-                "updateDate cannot be in the future"
+                'updateDate cannot be in the future'
             )
         return update_date
 
@@ -55,15 +55,28 @@ class ImportSchema(BaseModel):
         for unit in units:
             if unit.id in unit_types:
                 raise ValueError(
-                    f"id {unit.id} is not unique"
+                    f'id {unit.id} is not unique'
                 )
             unit_types.add(unit.id)
         return units
 
-    # @validator('items')
-    # def parent_ids_only_categories(cls, units):
-    #     raise ValueError("heyo")
+    @validator('items')
+    def parent_ids_only_categories(cls, units):
+        units_types = {}
+        parent_ids = set()
+        for unit in units:
+            units_types[unit.id] = unit.type
+            if unit.parentId is not None:
+                parent_ids.add(unit.parentId)
+        for parent_id in parent_ids:
+            if parent_id in units_types:
+                if units_types[parent_id] == ShopUnitType.OFFER:
+                    raise ValueError(
+                        'only categories can be parents'
+                    )
+        return units
+
 
 class ErrorSchema(BaseModel):
-    code: int
+    code: int = 400
     message: str
